24558f8ee74556261d9dbe0db74bec19a3e0b174
Nick Hollins <nholl22@wgu.edu>
2021-05-31 11:46:18 -0400

    removed regular string quotes and added template strings

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@
 
 // Generating days of the month
 for (day = 0; day < 32; day++) {
-    document.getElementById('day').innerHTML += `'<option>${day}</option>'`;
+    document.getElementById('day').innerHTML += `<option>${day}</option>`;
 }
