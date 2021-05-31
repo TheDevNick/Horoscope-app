67dfb05499a9351706c6d98fd9f2c0e7376ddc1b
Nick Hollins <nholl22@wgu.edu>
2021-05-31 11:48:52 -0400

    for loop was starting at 0 but 0 is not a day so I started the count at 1 for more accuracy

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Birthday months
 
 // Generating days of the month
-for (day = 0; day < 32; day++) {
+for (day = 1; day < 32; day++) {
     document.getElementById('day').innerHTML += `<option>${day}</option>`;
 }
