b068cf45cd9aedd088754579c88270109f8f93d8
Nick Hollins <nholl22@wgu.edu>
2021-06-02 16:45:03 -0400

    added more functionality and styling

--- js/main.js
+++ js/main.js
@@ -21,18 +21,32 @@ submit.addEventListener('click', function() {
 
     let birthDay = document.querySelector('#day').value;
 
-    // console.log(birthDay);
-    // console.log(monthValue);
 
-    // for (i = 0; i < birthdays.length; i++) {
-    //     if (monthValue === birthdays[0]) {
-    //         console.log('worked!!');
-    //         break;
-    //     }
-    // }
-
-    if (monthValue == birthdays[0] && birthDay < 10) {
-        console.log('Jan');
+    // It's a better way to do this but this is the first solution that comes to mind
+    if (monthValue == birthdays[0] && birthDay >= 20 || monthValue == birthdays[1] && birthDay <= 18) {
+        console.log('Aquarius');
+    } else if (monthValue == birthdays[1] && birthDay >= 19 || monthValue == birthdays[2] && birthDay <= 20) {
+        console.log('Pisces');
+    } else if (monthValue == birthdays[2] && birthDay >= 21 || monthValue == birthdays[3] && birthDay <= 19) {
+        console.log('Aries');
+    } else if (monthValue == birthdays[3] && birthDay >= 20 || monthValue == birthdays[4] && birthDay <= 20){
+        console.log('Taurus');
+    } else if (monthValue == birthdays[4] && birthDay >= 21 || monthValue == birthdays[5] && birthDay <= 20) {
+        console.log('Gemini');
+    } else if (monthValue == birthdays[5] && birthDay >= 21 || monthValue == birthdays[6] && birthDay <= 22) {
+        console.log('Cancer');
+    }  else if (monthValue == birthdays[6] && birthDay >= 23 || monthValue == birthdays[7] && birthDay <= 22) {
+        console.log('Leo');
+    } else if (monthValue == birthdays[7] && birthDay >= 23 || monthValue == birthdays[8] && birthDay <= 22) {
+        console.log('Virgo');
+    } else if (monthValue == birthdays[8] && birthDay >= 23 || monthValue == birthdays[9] && birthDay <= 22) {
+        console.log('Libra');
+    } else if (monthValue == birthdays[9] && birthDay >= 23 || monthValue == birthdays[10] && birthDay <= 21) {
+        console.log('Scorpio');
+    } else if (monthValue == birthdays[10] && birthDay >= 22 || monthValue == birthdays[11] && birthDay <= 21) {
+        console.log('Sagitarius');
+    } else if (monthValue == birthdays[11] && birthDay >= 22 || monthValue == birthdays[0] && birthDay <= 19) {
+        console.log('Capricorn');
     }
 });
 
