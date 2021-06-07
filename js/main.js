433ab01bbc9a5596412e88c92398e21e36043d84
Nick Hollins <hollinsaquile@gmail.com>
2021-06-07 10:42:37 -0400

    horoscope sign is displayed now instead of logged

--- js/main.js
+++ js/main.js
@@ -25,28 +25,40 @@ submit.addEventListener('click', function() {
     // It's a better way to do this but this is the first solution that comes to mind
     if (monthValue == birthdays[0] && birthDay >= 20 || monthValue == birthdays[1] && birthDay <= 18) {
         console.log('Aquarius');
+        document.getElementById('view').innerHTML = '<h3>Aquarius</h3>';
     } else if (monthValue == birthdays[1] && birthDay >= 19 || monthValue == birthdays[2] && birthDay <= 20) {
         console.log('Pisces');
+        document.getElementById('view').innerHTML = '<h3>Pisces</h3>';
     } else if (monthValue == birthdays[2] && birthDay >= 21 || monthValue == birthdays[3] && birthDay <= 19) {
         console.log('Aries');
+        document.getElementById('view').innerHTML = '<h3>Aries</h3>';
     } else if (monthValue == birthdays[3] && birthDay >= 20 || monthValue == birthdays[4] && birthDay <= 20){
         console.log('Taurus');
+        document.getElementById('view').innerHTML = '<h3>Taurus</h3>';
     } else if (monthValue == birthdays[4] && birthDay >= 21 || monthValue == birthdays[5] && birthDay <= 20) {
         console.log('Gemini');
+        document.getElementById('view').innerHTML = '<h3>Gemini</h3>';
     } else if (monthValue == birthdays[5] && birthDay >= 21 || monthValue == birthdays[6] && birthDay <= 22) {
         console.log('Cancer');
+        document.getElementById('view').innerHTML = '<h3>Cancer</h3>';
     }  else if (monthValue == birthdays[6] && birthDay >= 23 || monthValue == birthdays[7] && birthDay <= 22) {
         console.log('Leo');
+        document.getElementById('view').innerHTML = '<h3>Leo</h3>';
     } else if (monthValue == birthdays[7] && birthDay >= 23 || monthValue == birthdays[8] && birthDay <= 22) {
         console.log('Virgo');
+        document.getElementById('view').innerHTML = '<h3>Virgo</h3>';
     } else if (monthValue == birthdays[8] && birthDay >= 23 || monthValue == birthdays[9] && birthDay <= 22) {
         console.log('Libra');
+        document.getElementById('view').innerHTML = '<h3>Libra</h3>';
     } else if (monthValue == birthdays[9] && birthDay >= 23 || monthValue == birthdays[10] && birthDay <= 21) {
         console.log('Scorpio');
+        document.getElementById('view').innerHTML = '<h3>Scorpio</h3>';
     } else if (monthValue == birthdays[10] && birthDay >= 22 || monthValue == birthdays[11] && birthDay <= 21) {
         console.log('Sagitarius');
+        document.getElementById('view').innerHTML = '<h3>Sagitarius</h3>';
     } else if (monthValue == birthdays[11] && birthDay >= 22 || monthValue == birthdays[0] && birthDay <= 19) {
         console.log('Capricorn');
+        document.getElementById('view').innerHTML = '<h3>Capricorn</h3>';
     }
 });
 
